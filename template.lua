dc9d898b9ea6b6bc6e2c3b3ad04271a7f7a150d2
Marthenil <40279977+Marthenil@users.noreply.github.com>
2022-11-13 17:46:45 +0200

    Delete template.lua

--- template.lua
+++
@@ -1,5 +0,0 @@
-return {
-["character_subtype_key_1"] = {"character_01_art_set_01","character_01_art_set_02","character_01_art_set_03"},
-["character_subtype_key_2"] = {"character_02_art_set_01","character_02_art_set_02","character_02_art_set_03"}
-};
-
